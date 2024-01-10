8a64f40d02c4ee7728e80db6ab816ad50ef15dcf
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-01-09 22:42:43 -0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Main from './components/Main';
+import Succes from './components/Succes';
+import{Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <Main />
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/success' element={<Succes />} />
+    </Routes>
     </div>
   );
 }
---
+++ src/components/Succes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './sucess.module.css'
+
+function Succes() {
+  return (
+    <div>Succes</div>
+  )
+}
+
+export default Succes
